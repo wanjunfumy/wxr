baf6fc6d0054694907a761e0935f1309e65f88a5
wanjf <123456>
2020-06-12 10:53:31 +0800

    上传一个有hover的listView

---
+++ app/src/main/java/com/wanjf/mysecretapp/myViews/hoverListView/Body.java
@@ -0,0 +1,5 @@
+package com.wanjf.mysecretapp.myViews.hoverListView;
+
+public abstract class Body {
+    // I am just empty
+}
---
+++ app/src/main/java/com/wanjf/mysecretapp/myViews/hoverListView/Hover.java
@@ -0,0 +1,5 @@
+package com.wanjf.mysecretapp.myViews.hoverListView;
+
+public abstract class Hover {
+    // I am just empty
+}
